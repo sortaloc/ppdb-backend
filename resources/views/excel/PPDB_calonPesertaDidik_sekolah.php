5587e86f3a9850375cbd10c61c74419a42b8f389
ARF <arif@nufaza.com>
2020-06-03 13:52:02 +0700

    ubah kolom

--- resources/views/excel/PPDB_calonPesertaDidik_sekolah.php
+++ resources/views/excel/PPDB_calonPesertaDidik_sekolah.php
@@ -50,41 +50,40 @@ $table_header = array(
   ),
 );
 
-// $object->getActiveSheet()->getStyle('A1:N1')->applyFromArray($style_header);
-$object->getActiveSheet()->getStyle('A'.$headerTable.':N'.$headerTable)->applyFromArray($table_header);
+// $object->getActiveSheet()->getStyle('A1:M1')->applyFromArray($style_header);
+$object->getActiveSheet()->getStyle('A'.$headerTable.':M'.$headerTable)->applyFromArray($table_header);
 
 //set Width Colum
 $object->getActiveSheet()->getColumnDimension('A')->setWidth(7);
 $object->getActiveSheet()->getColumnDimension('B')->setWidth(20);
 $object->getActiveSheet()->getColumnDimension('C')->setWidth(18);
-$object->getActiveSheet()->getColumnDimension('D')->setWidth(17);
-$object->getActiveSheet()->getColumnDimension('E')->setWidth(3);
+$object->getActiveSheet()->getColumnDimension('D')->setWidth(9);
+$object->getActiveSheet()->getColumnDimension('E')->setWidth(4);
 $object->getActiveSheet()->getColumnDimension('F')->setWidth(13);
 $object->getActiveSheet()->getColumnDimension('G')->setWidth(12);
 $object->getActiveSheet()->getColumnDimension('H')->setWidth(9);
 $object->getActiveSheet()->getColumnDimension('I')->setWidth(7);
-$object->getActiveSheet()->getColumnDimension('J')->setWidth(9);
-$object->getActiveSheet()->getColumnDimension('K')->setWidth(11);
-$object->getActiveSheet()->getColumnDimension('L')->setWidth(18);
-$object->getActiveSheet()->getColumnDimension('M')->setWidth(15);
-$object->getActiveSheet()->getColumnDimension('N')->setWidth(14);
+$object->getActiveSheet()->getColumnDimension('J')->setWidth(11);
+$object->getActiveSheet()->getColumnDimension('K')->setWidth(18);
+$object->getActiveSheet()->getColumnDimension('L')->setWidth(15);
+$object->getActiveSheet()->getColumnDimension('M')->setWidth(14);
 
 //set Height Row
 // $object->getActiveSheet()->getRowDimension('4')->setRowHeight(8);
 
 //set font bold
 $object->getActiveSheet()->getStyle('B2')->getFont()->setBold(true);
-$object->getActiveSheet()->getStyle('A'.$headerTable.':N'.$headerTable)->getFont()->setBold(true);
+$object->getActiveSheet()->getStyle('A'.$headerTable.':M'.$headerTable)->getFont()->setBold(true);
 
 //set margeCell
-$object->getActiveSheet()->mergeCells('B1:N1');
-$object->getActiveSheet()->mergeCells('B2:N2');
-$object->getActiveSheet()->mergeCells('B3:N3');
-$object->getActiveSheet()->mergeCells('B4:N4');
+$object->getActiveSheet()->mergeCells('B1:M1');
+$object->getActiveSheet()->mergeCells('B2:M2');
+$object->getActiveSheet()->mergeCells('B3:M3');
+$object->getActiveSheet()->mergeCells('B4:M4');
 
 //set colum text center
 $object->getActiveSheet()->getStyle('A')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-$object->getActiveSheet()->getStyle('A'.$headerTable.':N'.$headerTable)->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+$object->getActiveSheet()->getStyle('A'.$headerTable.':M'.$headerTable)->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
 
 //set colom text LEFT
 // $object->getActiveSheet()->getStyle('A1:C10')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_LEFT);
@@ -107,7 +106,7 @@ $object->setActiveSheetIndex(0)
 ->setCellValue('B1', 'DAFTAR CALON PESERTA DIDIK BARU KAB. LUMAJANG TAHUN AJARAN 2020/2021')
 ->setCellValue('B2', $sekolah->nama)
 ->setCellValue('B3', 'Alamat: '.$sekolah->alamat_jalan." ".$sekolah->kabupaten." ".$sekolah->kecamatan)
-->setCellValue('B4', 'KUOTA :  '.$sekolah->kuota.' Siswa (1 Rombel)')
+->setCellValue('B4', 'KUOTA :  '.$sekolah->kuota.' Siswa')
 ->setCellValue('A'.$headerTable, 'No')
 ->setCellValue('B'.$headerTable, 'Nama Siswa')
 ->setCellValue('C'.$headerTable, 'NIK')
@@ -117,14 +116,13 @@ $object->setActiveSheetIndex(0)
 ->setCellValue('G'.$headerTable, 'Tanggal Lahir')
 ->setCellValue('H'.$headerTable, 'Jarak ke Sekolah (meter)')
 ->setCellValue('I'.$headerTable, 'Pilihan')
-->setCellValue('J'.$headerTable, 'Rangking Diterima')
-->setCellValue('K'.$headerTable, 'Tanggal Disetujui')
-->setCellValue('L'.$headerTable, 'Status')
-->setCellValue('M'.$headerTable, 'Orangtua')
-->setCellValue('N'.$headerTable, 'Kontak Orangtua')
+->setCellValue('J'.$headerTable, 'Tanggal Disetujui')
+->setCellValue('K'.$headerTable, 'Status')
+->setCellValue('L'.$headerTable, 'Orangtua')
+->setCellValue('M'.$headerTable, 'Kontak Orangtua')
 ;
 
-$object->getActiveSheet()->getStyle('A'.$headerTable.':N'.$headerTable)->applyFromArray($style_row);
+$object->getActiveSheet()->getStyle('A'.$headerTable.':M'.$headerTable)->applyFromArray($style_row);
 
 //Table
 $num = $headerTable + 1;
@@ -143,16 +141,15 @@ foreach ($daftarPengawas as $value) {
   $ex->setCellValue("G".$num, date("d/m/Y", strtotime($value->tanggal_lahir)));
   $ex->setCellValue("H".$num, intval($value->jarak));
   $ex->setCellValue("I".$num, $value->urut_pilihan);
-  $ex->setCellValue("J".$num, "-");
-  $ex->setCellValue("K".$num, date("d/m/Y", strtotime($value->tanggal_konfirmasi)));
-  $ex->setCellValue("L".$num, $value->status_terima !== null ? $value->status_terima : "-");
-  $ex->setCellValue("M".$num, $value->$nama_orangtua);
-  $ex->setCellValue("N".$num, $value->$no_telepon_orangtua);
+  $ex->setCellValue("J".$num, date("d/m/Y", strtotime($value->tanggal_konfirmasi)));
+  $ex->setCellValue("K".$num, $value->status_terima !== null ? $value->status_terima : "-");
+  $ex->setCellValue("L".$num, $value->$nama_orangtua);
+  $ex->setCellValue("M".$num, $value->$no_telepon_orangtua);
 
-  $object->getActiveSheet()->getStyle('A'.$num.':N'.$num)->applyFromArray($style_row);
-  $object->getActiveSheet()->getStyle('A'.$num.':N'.$num)->getFont()->setSize(9);
+  $object->getActiveSheet()->getStyle('A'.$num.':M'.$num)->applyFromArray($style_row);
+  $object->getActiveSheet()->getStyle('A'.$num.':M'.$num)->getFont()->setSize(9);
 
-  $object->getActiveSheet()->getStyle('A'.$num.':N'.$num)->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+  $object->getActiveSheet()->getStyle('A'.$num.':M'.$num)->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
   $object->getActiveSheet()->getStyle('B'.$num)->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_LEFT);
   $object->getActiveSheet()->getStyle("C".$num)->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_TEXT);
   $object->getActiveSheet()->getStyle("C".$num)->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_NUMBER);
@@ -161,7 +158,7 @@ foreach ($daftarPengawas as $value) {
   $no++;
 }
 
-$object->getActiveSheet()->getStyle('A1:N'.$object->getActiveSheet()->getHighestRow())->getAlignment()->setWrapText(true); 
+$object->getActiveSheet()->getStyle('A1:M'.$object->getActiveSheet()->getHighestRow())->getAlignment()->setWrapText(true); 
 
 // /--------------
 
